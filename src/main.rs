047bea5cad0ef0ecb6d185d08555a6678c54e37e
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2024-03-16 17:10:01 -0400

    Memory Acquisition (Unbeautified)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use sysinfo::System;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting System Information Grab...");
+    grab_memory();
+}
+
+fn grab_memory() {
+    let mut sys = System::new_all();
+    sys.refresh_memory();
+
+    println!("System Memory Info: ");
+    println!("  Total Memory: {} bytes", sys.total_memory());
+    println!("  Memory In Use: {} bytes", sys.used_memory());
+    println!("  Total Swap: {} bytes", sys.total_swap());
+    println!("  Swap In Use: {} bytes", sys.used_swap());
 }
